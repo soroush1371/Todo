b7d3b3a7c6cde285e3f14115aa050a99d3f1ebb2
Soroush <soroushshahnazian@gmail.com>
2020-01-10 20:42:53 +0330

    Updates

---
+++ precache-manifest.70ae5472d86d2074146f2713fa9ce520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553b0ad838986ebae46431ee85cb16da",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "38a11fc5f5f338113b41",
+    "url": "/Todo/static/css/main.32c75c16.chunk.css"
+  },
+  {
+    "revision": "82f4a21401a4cd3f1f6e",
+    "url": "/Todo/static/js/2.997eaa9e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo/static/js/2.997eaa9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38a11fc5f5f338113b41",
+    "url": "/Todo/static/js/main.ce4be9f4.chunk.js"
+  },
+  {
+    "revision": "048c095e4b628c8aaf16",
+    "url": "/Todo/static/js/runtime-main.235257a6.js"
+  }
+]);
